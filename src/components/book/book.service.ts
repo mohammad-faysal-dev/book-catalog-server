544e59d44859182e9a6af831e2df1903eea1209e
Faysal2123 <foysal2005n1@gmail.com>
2026-06-30 21:50:26 +0600

    feat: implement update book service

--- src/components/book/book.service.ts
+++ src/components/book/book.service.ts
@@ -14,8 +14,18 @@ const getSingleBook = async (id: string | undefined): Promise<IBook | null> => {
   const result = await Book.findById(id);
   return result;
 };
+const updateBook = async (
+  id: string | undefined,
+  payload: Partial<IBook>,
+): Promise<IBook | null> => {
+  const result = await Book.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
 export const BookService = {
   getAllBooks,
   getSingleBook,
   createBook,
+  updateBook,
 };
