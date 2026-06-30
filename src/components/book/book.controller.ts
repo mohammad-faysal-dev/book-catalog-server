a8c340a7aec98f6cd8855348427f29eb82036e20
Faysal2123 <foysal2005n1@gmail.com>
2026-06-30 19:50:48 +0600

    feat(book): add create book controller

--- src/components/book/book.controller.ts
+++ src/components/book/book.controller.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
 import { BookService } from "./book.service";
 
+const createBook = async (req: Request, res: Response) => {
+  const { ...bookData } = req.body;
+  const result = await BookService.createBook(bookData);
+  res.json({
+    statusCode: 200,
+    success: true,
+    message: "Book create successfully",
+    data: result,
+  });
+};
+
 const getAllBooks = async (req: Request, res: Response) => {
   const result = await BookService.getAllBooks();
   res.json({
@@ -25,4 +36,5 @@ const getSingleBook = async (req: Request, res: Response) => {
 export const BooksController = {
   getAllBooks,
   getSingleBook,
+  createBook
 };
